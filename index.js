e3ec9514ae39c68b2829a8b8fb4e495b8dee3120
maxi-morini <pabloadpe@gmail.com>
2024-06-10 15:28:05 -0300

    validacion mongoose

--- index.js
+++ index.js
@@ -10,12 +10,8 @@ app.use(express.static('dist'))
 
 const pass = process.argv[2]
 
-// const url = `mongodb+srv://pabloadpe:${pass}@cluster0.juw9yhi.mongodb.net/noteApp?
-//             retryWrites=true&w=majority`
-
 
 mongoose.set('strictQuery',false)
-// mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
     content : String,
@@ -30,7 +26,6 @@ noteSchema.set('toJSON',{
 })
 
 
-//const Note = mongoose.model('Note',noteSchema)
 
 let notes = [
     {
@@ -56,21 +51,43 @@ app.use(cors())
 app.get('/',(req, res)=>{
     res.send('<h1>Hello worldango</h1>')
 }) 
-app.get('/api/notes',(req,res)=>{
+app.post('/api/notes',(req, res, next)=>{
+    const body = req.body
+    console.log(req.body)
+    const note = new Note({
+        content: body.content,
+        important: body.important || false,
+    })
+    
+    note.save()
+    .then(savedNote => {
+        res.json(savedNote)
+    })
+    .catch(err=> next(err))
+})
+app.get('/api/notes',(req ,res ,next)=>{
     Note.find({}).then(notes=>{
         res.json(notes)
     })   
 })
 app.get('/api/notes/:id',(req,res)=>{
-    const id =Number(req.params.id)
-    const note = notes.find(n => n.id===id)
-    note ? res.json(note) : res.status(404).end()
+    Note.findById(request.params.id)
+    .then(note => {
+        if(note){
+            res.json(note)
+        }
+        else{
+            res.status(404).end()
+        }
+      })
+    .catch( err =>next(err))
 })
-app.delete('/api/notes/:id',(req,res)=>{
-    const id =Number(req.params.id)
-    notes = notes.filter(n => n.id===id)
-    res.status(204).end()
-
+app.delete('/api/notes/:id',( req , res, next)=>{
+    Note.findByIdAndDelete(req.params.id)
+    .then(result =>{
+        res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const generadorId = () =>{
@@ -79,35 +96,20 @@ const generadorId = () =>{
 }
 
 
-app.post('/api/notes',(req,res)=>{
 
-    const body  = req.body
-    if(!body.content){
-        return res.status(400).json({error: 'contenido perdido'})
-    }
+app.put('/api/notes/:id',(req,res, next)=>{
+    
+    const {content , important } =req.body
 
-    const note = {
-        content : body.content,
-        important : body.important || false,
-        id : generadorId(),
-    }
-
-    notes = notes.concat(note)
-    res.json(note)
-
-})
-
-app.put('/api/notes/:id',(req,res)=>{
-    const id =Number(req.params.id)
-    const body = req.body
-
-    const newObj = {
-        id : body.id,
-        content : body.content,
-        important : body.important
-    }
-    notes = notes.map(n => n.id !== id? n : newObj)
-    res.json(notes)
+    Note.findByIdAndUpdate(
+        req.params.id,
+        {content , important },
+        {new : true, runValidators : true, context: 'query' }
+    )
+    .then(updatedNote => {
+        response.json(updatedNote)
+      })
+      .catch(error => next(error))
 })
 
 const endPointDesconocido = (req,res)=>{
@@ -116,6 +118,19 @@ const endPointDesconocido = (req,res)=>{
 
 app.use(endPointDesconocido)
 
+const errorHandler = ( error , request , response , next ) => {
+    console.error(error.message)
+
+    if(error.name === "CastError") {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+    else if(error.name === "ValidationError"){
+        return response.status(400).json({ error: error.message })
+    }
+    
+    next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => 
